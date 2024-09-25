8bfc2b2d84cdb55b77c650a05a4a9569dac19b6b
JUNGHOON AN <gnstm831@naver.com>
2024-09-25 19:20:22 +0900

    섹션3. JPA 시작하기
    1. Hello JPA - 프로젝트 생성
    2. Hello JPA - 애플리케이션 개발

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,46 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        /* EntityManagerFactory는 로딩 시점에 단 하나만 만들어야 됨 */
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        /* JPA에서는 모든 데이터를 변경하는 작업은 꼭 Transaction 안에서 수행해야 됨 */
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        /* code 작성 */
+        try {
+            /* JPA 사용 */
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            /* JPQL 사용 */
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
