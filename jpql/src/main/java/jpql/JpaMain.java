0ac9f95b049ce396e937d0c502e68f1585f50b9c
JUNGHOON AN <gnstm831@naver.com>
2024-10-14 15:14:55 +0900

    섹션11. 객체지향 쿼리 언어1 - 기본문법
    2. 기본 문법과 쿼리 API
    3. 프로젝션(SELECT)
    4. 페이징
    5. 조인

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,51 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        /* EntityManagerFactory는 로딩 시점에 단 하나만 만들어야 됨 */
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        /* JPA에서는 모든 데이터를 변경하는 작업은 꼭 Transaction 안에서 수행해야 됨 */
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        /* code 작성 */
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+
+            member.changeTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
