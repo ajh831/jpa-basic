f9ce94eb554d175eec4de58f73470d07853f4c35
JUNGHOON AN <gnstm831@naver.com>
2024-10-14 09:37:42 +0900

    섹션11. 객체지향 쿼리 언어1 - 기본문법
    1. 소개

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Hibernate;
 
 import javax.swing.*;
@@ -24,45 +27,15 @@ public class JpaMain {
 
         /* code 작성 */
         try {
+
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            // flush 시점 -> commit, query
 
-            System.out.println("================ START ================");
-            Member findMember = em.find(Member.class, member.getId());
-
-/*            // homeCity -> newCity
-            *//*
-               findMember.getHomeAddress().setCity("newCity");
-               이렇게 값을 수정하면 안됨 -> 사이드 이펙트 발생
-               값 타입은 immutable해야 됨
-            *//*
-            Address oldAddress = findMember.getHomeAddress();
-
-            // 새로운 인스턴스로 교체해야 됨
-            findMember.setHomeAddress(new Address("newCity", oldAddress.getStreet(), oldAddress.getZipcode()));
-
-            // 값 타입 컬렉션 수정
-            // 치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");*/
-
-            // old1 -> newCity1
-            // 기본적으로 컬렉션들은 대상을 찾을 때 equals를 사용하기 때문에 equals와 hashcode가 재정의 되어 있어야한다.
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
